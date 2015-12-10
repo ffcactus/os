7eb70beb063eeac4a009d41edf104f1f5d1a13c2
bin.bai <bin.bai@hp.com>
2015-12-10 16:05:55 +0800

    We can embrace the C language now!
    The binary could contain the .ds section. The failure is due to the
    size of the binary. There is a big gap between the code and read only
    data in the binary. Hence the size of the binary is over than 2Kb now.
    Our bootloader assume the size of the binary is less than 2Kb. I also
    found although the sector size of the CD is 2Kb, the BIOS read 512b
    each time. After fix all of these, we can embrace the C language now!

--- boot/init/main.c
+++ boot/init/main.c
@@ -5,15 +5,12 @@
 #include <stdint.h>
 #include "terminal.h"
 
-uint32_t ds = 0x1234;
 /**
  * Take it as main().
  */
 void _start() {
-	uint32_t bss = 0x4321;
-	bss += ds;
-//	term_init();
-//	term_printf("xxxx");
-//	while(1);
+	term_init();
+	term_printf("xxxx");
+	while(1);
 }
 
